85efa09dc44096047929758882894de5d7ddea40
Thiago Cruz <thiagocruz.eu@gmail.com>
2026-01-26 16:02:45 -0300

    feat(logger): add configurable logger utility using Consola
    Introduces environment-based log level and standardizes logging with app-wide tags.

---
+++ lib/logger.utils.ts
@@ -0,0 +1,22 @@
+import { SYSTEM_CONSTANTS } from '@/core/domain/constants/system.constants';
+import { consola } from 'consola';
+
+/**
+ * Standard log levels:
+ * 0: Fatal & Error
+ * 1: Warnings
+ * 2: Informational messages
+ * 3: Debug messages
+ * 4: Trace messages
+ * 5: All logs
+ */
+const isProduction = process.env.NODE_ENV === 'production';
+
+export const logger = consola.create({
+  level: isProduction ? 1 : 4,
+  defaults: {
+    tag: SYSTEM_CONSTANTS.ACRONYM,
+  },
+});
+
+export default logger;
